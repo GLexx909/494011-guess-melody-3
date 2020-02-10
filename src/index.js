c242476464e4ca45f4e81747f1dfc0b890d56c76
Vitaly Simonov <guardian909@rambler.ru>
2020-02-10 13:01:46 +0300

    Edit WelcomeScreen

--- src/index.js
+++ src/index.js
@@ -2,9 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app/app';
 
-const errors = 3;
+const ERRORS = 3;
 
 ReactDOM.render(
-    <App errors={errors}/>,
+    <App errors={ERRORS}/>,
     document.getElementById(`root`)
 );
